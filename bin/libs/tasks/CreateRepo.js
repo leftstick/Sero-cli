86eec23e6c5d47b2027734609b9a38cf238f4e62
Howard.Zuo <leftstick@qq.com>
2014-06-30 19:34:21 +0800

    fix issues that phonegap plugins can be injected into package.json for the created project

--- bin/libs/tasks/CreateRepo.js
+++ bin/libs/tasks/CreateRepo.js
@@ -55,8 +55,8 @@ var createRepo = function(opts, defer, answer) {
             var scaffoldPath = path.join(__dirname, '../gapScaffold');
 
             var stream = gulp.src(scaffoldPath + '/**/*')
-            .pipe(template(_.assign(answer, res)))
-            .pipe(gulp.dest(answer.localPath));
+                .pipe(template(_.assign(answer, res)))
+                .pipe(gulp.dest(answer.localPath));
 
             stream.on('close', defer.resolve);
             stream.on('finish', defer.resolve);
@@ -79,148 +79,140 @@ Task.prototype.start = function() {
     var d = Q.defer();
 
     inquirer.prompt([{
-            type: 'input',
-            name: 'username',
-            message: 'Username or Email for Github',
-            default: process.env.USERNAME
-        }, {
-            type: 'password',
-            name: 'password',
-            message: 'Password for Github',
-            validate: function(pass) {
-                return !!pass;
+        type: 'input',
+        name: 'username',
+        message: 'Username or Email for Github',
+        default: process.env.USERNAME
+    }, {
+        type: 'password',
+        name: 'password',
+        message: 'Password for Github',
+        validate: function(pass) {
+            return !!pass;
+        }
+    }, {
+        type: 'input',
+        name: 'proName',
+        message: 'Project name to be created',
+        validate: function(pass) {
+            return !!pass;
+        }
+    }, {
+        type: 'input',
+        name: 'proDesc',
+        message: 'Project description'
+    }, {
+        type: 'input',
+        name: 'proAuthor',
+        message: 'Project Author\'s name',
+        default: process.env.USERNAME,
+        validate: function(pass) {
+            return !!pass;
+        }
+    }, {
+        type: 'input',
+        name: 'proEmail',
+        message: 'Project Author\'s email',
+        default: function(answer) {
+            if (mail_reg.test(answer.username)) {
+                return answer.username;
             }
+        },
+        validate: function(pass) {
+            return mail_reg.test(pass);
+        }
+    }, {
+        type: 'input',
+        name: 'proWebsite',
+        message: 'Project Author\'s website'
+    }, {
+        type: 'input',
+        name: 'widgetId',
+        message: 'Project\'s namespace',
+        default: 'com.example',
+        validate: function(pass) {
+            var re = /^[a-z]+([.][a-z]+)+$/;
+            return re.test(pass);
+        }
+    }, {
+        type: 'checkbox',
+        name: 'plugins',
+        message: 'Project\'s plugins',
+        default: 'com.example',
+        choices: [{
+            name: 'Battery Status',
+            value: 'org.apache.cordova.battery-status',
+            checked: true
         }, {
-            type: 'input',
-            name: 'proName',
-            message: 'Project name to be created',
-            validate: function(pass) {
-                return !!pass;
-            }
+            name: 'Camera',
+            value: 'org.apache.cordova.camera',
+            checked: true
         }, {
-            type: 'input',
-            name: 'proDesc',
-            message: 'Project description',
-            validate: function(pass) {
-                return !!pass;
-            }
+            name: 'Console',
+            value: 'org.apache.cordova.console',
+            checked: true
         }, {
-            type: 'input',
-            name: 'proAuthor',
-            message: 'Project Author\'s name',
-            default: process.env.USERNAME,
-            validate: function(pass) {
-                return !!pass;
-            }
+            name: 'Contacts',
+            value: 'org.apache.cordova.contacts',
+            checked: true
         }, {
-            type: 'input',
-            name: 'proEmail',
-            message: 'Project Author\'s email',
-            default: function(answer) {
-                if (mail_reg.test(answer.username)) {
-                    return answer.username;
-                }
-            },
-            validate: function(pass) {
-                return mail_reg.test(pass);
-            }
+            name: 'Device',
+            value: 'org.apache.cordova.device',
+            checked: true
         }, {
-            type: 'input',
-            name: 'proWebsite',
-            message: 'Project Author\'s website',
-            validate: function(pass) {
-                return !!pass;
-            }
+            name: 'Device Motion (Accelerometer)',
+            value: 'org.apache.cordova.device-motion',
+            checked: true
         }, {
-            type: 'input',
-            name: 'widgetId',
-            message: 'Project\'s namespace',
-            default: 'com.example',
-            validate: function(pass) {
-                var re = /^[a-z]+([.][a-z]+)+$/;
-                return re.test(pass);
-            }
+            name: 'Device Orientation (Compass)',
+            value: 'org.apache.cordova.device-orientation',
+            checked: true
         }, {
-            type: 'checkbox',
-            name: 'plugins',
-            message: 'Project\'s plugins',
-            default: 'com.example',
-            choices: [
-                {
-                    name: 'Battery Status',
-                    value: 'org.apache.cordova.battery-status',
-                    checked: true
-                }, {
-                    name: 'Camera',
-                    value: 'org.apache.cordova.camera',
-                    checked: true
-                }, {
-                    name: 'Console',
-                    value: 'org.apache.cordova.console',
-                    checked: true
-                }, {
-                    name: 'Contacts',
-                    value: 'org.apache.cordova.contacts',
-                    checked: true
-                }, {
-                    name: 'Device',
-                    value: 'org.apache.cordova.device',
-                    checked: true
-                }, {
-                    name: 'Device Motion (Accelerometer)',
-                    value: 'org.apache.cordova.device-motion',
-                    checked: true
-                }, {
-                    name: 'Device Orientation (Compass)',
-                    value: 'org.apache.cordova.device-orientation',
-                    checked: true
-                }, {
-                    name: 'Dialogs',
-                    value: 'org.apache.cordova.dialogs',
-                    checked: true
-                }, {
-                    name: 'FileSystem',
-                    value: 'org.apache.cordova.file',
-                    checked: true
-                }, {
-                    name: 'File Transfer',
-                    value: 'org.apache.cordova.file-transfer',
-                    checked: true
-                }, {
-                    name: 'Geolocation',
-                    value: 'org.apache.cordova.geolocation',
-                    checked: true
-                }, {
-                    name: 'Globalization',
-                    value: 'org.apache.cordova.globalization',
-                    checked: true
-                }, {
-                    name: 'InAppBrowser',
-                    value: 'org.apache.cordova.inappbrowser',
-                    checked: true
-                }, {
-                    name: 'Media',
-                    value: 'org.apache.cordova.media',
-                    checked: true
-                }, {
-                    name: 'Media Capture',
-                    value: 'org.apache.cordova.media-capture',
-                    checked: true
-                }, {
-                    name: 'Network Information (Connection)',
-                    value: 'org.apache.cordova.network-information',
-                    checked: true
-                }, {
-                    name: 'Splashscreen',
-                    value: 'org.apache.cordova.splashscreen',
-                    checked: true
-                }, {
-                    name: 'Vibration',
-                    value: 'org.apache.cordova.vibration',
-                    checked: true
-                }
-            ]
-        }], function(res) {
+            name: 'Dialogs',
+            value: 'org.apache.cordova.dialogs',
+            checked: true
+        }, {
+            name: 'FileSystem',
+            value: 'org.apache.cordova.file',
+            checked: true
+        }, {
+            name: 'File Transfer',
+            value: 'org.apache.cordova.file-transfer',
+            checked: true
+        }, {
+            name: 'Geolocation',
+            value: 'org.apache.cordova.geolocation',
+            checked: true
+        }, {
+            name: 'Globalization',
+            value: 'org.apache.cordova.globalization',
+            checked: true
+        }, {
+            name: 'InAppBrowser',
+            value: 'org.apache.cordova.inappbrowser',
+            checked: true
+        }, {
+            name: 'Media',
+            value: 'org.apache.cordova.media',
+            checked: true
+        }, {
+            name: 'Media Capture',
+            value: 'org.apache.cordova.media-capture',
+            checked: true
+        }, {
+            name: 'Network Information (Connection)',
+            value: 'org.apache.cordova.network-information',
+            checked: true
+        }, {
+            name: 'Splashscreen',
+            value: 'org.apache.cordova.splashscreen',
+            checked: true
+        }, {
+            name: 'Vibration',
+            value: 'org.apache.cordova.vibration',
+            checked: true
+        }]
+    }], function(res) {
         var opts = _.clone(options);
         opts.auth.user = res.username;
         opts.auth.pass = res.password;
