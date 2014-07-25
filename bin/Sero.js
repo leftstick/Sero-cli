44557b3a1f5aad595dd8a05645021cf11723a801
Howard.Zuo <leftstick@qq.com>
2014-07-25 09:45:25 +0800

    add upgrade notifier

--- bin/Sero.js
+++ bin/Sero.js
@@ -4,6 +4,19 @@
 
 var path = require('path');
 var TaskRunner = require('terminal-task-runner');
+var updateNotifier = require('update-notifier');
+var pkg = require('../package.json');
+
+
+
+// Checks for available update and returns an instance
+var notifier = updateNotifier({
+    packageName: pkg.name,
+    packageVersion: pkg.version,
+    updateCheckInterval: 1000 * 60 * 60
+});
+
+notifier.notify();
 
 var title = 'SERO SAVE YOU FROM COMPLICATED DEVELOPMENT WORKS';
 var subtitle = 'Select an task and hit Enter to begin';
