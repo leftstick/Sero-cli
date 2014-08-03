737b5002b18c15e6482383b2a3065127aebe349a
Howard.Zuo <leftstick@qq.com>
2014-08-03 10:59:17 +0800

    fix bug that createrepo task doesn't work

--- bin/tasks/createrepo/Task.js
+++ bin/tasks/createrepo/Task.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var fs = require('fs');
 var path = require('path');
 var inquirer = require('inquirer');
@@ -6,8 +8,6 @@ var TaskRunner = require('terminal-task-runner');
 var Shell = TaskRunner.shell;
 var Base = TaskRunner.Base;
 var logger = TaskRunner.logger;
-
-var MAIL_REG = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 
 var options = {
@@ -38,78 +38,80 @@ var Task = Base.extend({
     position: 5,
     run: function(cons) {
 
+        var _this = this;
+
         inquirer.prompt([{
-                type: 'input',
-                name: 'username',
-                message: 'Username or Email for Github',
-                default: _this.get('githubaccount', process.env.USERNAME)
+            type: 'input',
+            name: 'username',
+            message: 'Username or Email for Github',
+            default: _this.get('githubaccount', process.env.USERNAME)
             }, {
-                type: 'password',
-                name: 'password',
-                message: 'Password for Github',
-                validate: function(pass) {
-                    return !!pass;
-                }
+            type: 'password',
+            name: 'password',
+            message: 'Password for Github',
+            validate: function(pass) {
+                return !!pass;
+            }
             }, {
-                type: 'input',
-                name: 'proName',
-                message: 'Project name to be created',
-                validate: function(pass) {
-                    return !!pass;
-                }
+            type: 'input',
+            name: 'proName',
+            message: 'Project name to be created',
+            validate: function(pass) {
+                return !!pass;
+            }
             }, {
-                type: 'input',
-                name: 'proDesc',
-                message: 'Project description'
-            }], function(answer) {
-            options.auth.user = answer.username;
-            options.auth.pass = answer.password;
-            options.body.name = answer.proName;
-            options.body.description = answer.proDesc;
-            options.body = JSON.stringify(options.body);
+            type: 'input',
+            name: 'proDesc',
+            message: 'Project description'
+        }], function(answer) {
+                options.auth.user = answer.username;
+                options.auth.pass = answer.password;
+                options.body.name = answer.proName;
+                options.body.description = answer.proDesc;
+                options.body = JSON.stringify(options.body);
 
-            answer.localPath = path.join('.', answer.proName);
-            _this.put({
-                githubaccount: answer.username
-            });
+                answer.localPath = path.join('.', answer.proName);
+                _this.put({
+                    githubaccount: answer.username
+                });
 
 
-            fs.exists(answer.localPath, function(exists) {
-                if (exists) {
-                    cons('The directory [' + answer.proName + '] is already exist.');
-                    return;
-                }
-
-                request(options, function(err, incoming, response) {
-                    var res = JSON.parse(response);
-                    if (err) {
-                        cons(err);
+                fs.exists(answer.localPath, function(exists) {
+                    if (exists) {
+                        cons('The directory [' + answer.proName + '] is already exist.');
                         return;
                     }
-                    if (res.errors) {
-                        cons(res.errors[0].message);
-                        return;
-                    }
-                    if (res.message) {
-                        cons(res.message);
-                        return;
-                    }
-                    logger.success('Repository [' + answer.proName + '] is created!');
 
-                    var exec = new Shell(['git clone ' + res.ssh_url]);
+                    request(options, function(err, incoming, response) {
+                        var res = JSON.parse(response);
+                        if (err) {
+                            cons(err);
+                            return;
+                        }
+                        if (res.errors) {
+                            cons(res.errors[0].message);
+                            return;
+                        }
+                        if (res.message) {
+                            cons(res.message);
+                            return;
+                        }
+                        logger.success('Repository [' + answer.proName + '] is created!');
 
-                    exec.start().then(function() {
-                        cons();
-                        return;
-                    }, function(err) {
-                        cons(err);
-                        return;
+                        var exec = new Shell(['git clone ' + res.ssh_url]);
+
+                        exec.start().then(function() {
+                            cons();
+                            return;
+                        }, function(err) {
+                                cons(err);
+                                return;
+                            });
                     });
+
                 });
 
             });
-
-        });
 
 
     }
