ecf70f2a510a1924d1eb6c3841954a21aedfa0ec
Howard.Zuo <leftstick@qq.com>
2016-02-18 09:48:44 +0800

    update server sub-command with browsersync involved instead old livereload

--- bin/tasks/startwebserver/Task.js
+++ bin/tasks/startwebserver/Task.js
@@ -2,12 +2,17 @@
 var TaskRunner = require('terminal-task-runner');
 var Base = TaskRunner.Base;
 
-var isInt = function(val) {
+var gulp = require('gulp');
+var path = require('path');
+var fs = require('fs');
+var browserSync = require('browser-sync');
+var history = require('connect-history-api-fallback');
+
+var isInt = function(val){
     return !isNaN(parseInt(val));
 };
 
-var compileLess = function(lessPath, dest, paths) {
-    var gulp = require('gulp');
+var compileLess = function(lessPath, dest, paths){
     var less = require('gulp-less');
     var LessPluginAutoPrefix = require('less-plugin-autoprefix');
     var autoprefix = new LessPluginAutoPrefix({
@@ -20,10 +25,34 @@ var compileLess = function(lessPath, dest, paths) {
 
     gulp.src(lessPath + '/main.less')
         .pipe(plumber())
-        .pipe(less({paths: paths, plugins: [autoprefix]}))
+        .pipe(less({ paths: paths, plugins: [autoprefix] }))
         .pipe(gulp.dest(dest));
 
     TaskRunner.logger.info(lessPath + '/main.less was compiled');
+};
+
+var setupServer = function(answer){
+    var bs = browserSync.create();
+    var initOpts = {
+        ui: false,
+        server: {
+            baseDir: process.cwd(),
+            directory: false
+        },
+        files: [
+            './**/*.*'
+        ],
+        port: answer.port || 8080,
+        open: 'local',
+        reloadOnRestart: true,
+        injectChanges: true
+    };
+    if (answer.pushState){
+        initOpts.middleware = [history()];
+    }
+
+    bs.init(initOpts);
+    bs.reload();
 };
 
 
@@ -33,10 +62,6 @@ var Task = Base.extend({
     command: 'server',
     options: [
         {
-            flags: '-r, --root <root>',
-            description: 'specify the root path of the static webserver'
-        },
-        {
             flags: '-p, --port <port>',
             description: 'specify the port of the static webserver'
         },
@@ -45,35 +70,23 @@ var Task = Base.extend({
             description: 'specify the less paths'
         },
         {
-            flags: '-l, --livereload',
-            description: 'specify whether to enable livereload'
-        },
-        {
             flags: '-ps, --pushState',
             description: 'specify whether to enable html5 pust state mode'
         }
     ],
-    check: function(cmd) {
-        return cmd.root && cmd.port;
+    check: function(cmd){
+        return cmd.port;
     },
-    run: function(cons) {
+    run: function(cons){
         var _this = this;
-        var fs = require('fs');
-        var path = require('path');
 
         this.prompt([
-            {
-                type: 'input',
-                name: 'root',
-                message: 'root for webserver',
-                default: '.'
-            },
             {
                 type: 'input',
                 name: 'port',
                 message: 'port for webserver',
                 default: this.get('webserverport', 8080),
-                validate: function(pass) {
+                validate: function(pass){
                     return isInt(pass);
                 }
             },
@@ -81,8 +94,8 @@ var Task = Base.extend({
                 type: 'input',
                 name: 'paths',
                 message: 'paths for less(separate in comma)',
-                when: function(pass) {
-                    return fs.existsSync(path.join(pass.root, 'less'));
+                when: function(pass){
+                    return fs.existsSync(path.join(process.cwd(), 'less'));
                 }
             },
             {
@@ -90,78 +103,41 @@ var Task = Base.extend({
                 name: 'pushState',
                 message: 'would you like to enable html5 push state mode?',
                 default: true
-            },
-            {
-                type: 'confirm',
-                name: 'livereload',
-                message: 'would you like to have livereload?',
-                default: true
             }
-        ], function(answer) {
+        ], function(answer){
             _this.action(answer, cons);
         });
     },
-    action: function(answer, cons) {
+    action: function(answer, cons){
 
-        if (!isInt(answer.port)) {
-            cons('port must be int');
-            return;
+        if (!isInt(answer.port)){
+            return cons('port must be int');
         }
 
-        var gulp = require('gulp');
-        var path = require('path');
-        var fs = require('fs');
-        var webserver = require('gulp-webserver');
+        this.put({ webserverport: answer.port });
 
-        var _this = this;
+        var lessPath = path.join(process.cwd(), 'less');
+        var dest = path.join(process.cwd(), 'css');
 
+        var paths = answer.paths ? answer.paths.split(',')
+            .forEach(function(p){
+                paths.push(path.join(process.cwd(), p));
+            }) : [];
 
-        _this.put({webserverport: answer.port});
-
-        var lessPath = path.join(answer.root, 'less');
-        var dest = path.join(answer.root, 'css');
-
-        var paths = [];
-
-        if (answer.paths) {
-            answer.paths.split(',').forEach(function(p) {
-                paths.push(path.join(answer.root, p));
-            });
-        }
-
-        fs.exists(lessPath, function(exists) {
-            if (!exists) {
+        fs.exists(lessPath, function(exists){
+            if (!exists){
                 return;
             }
             compileLess(lessPath, dest, paths);
         });
 
-        var stream = gulp.src(answer.root)
-            .pipe(webserver({
-                host: '0.0.0.0',
-                port: answer.port,
-                livereload: answer.livereload,
-                directoryListing: false,
-                fallback: answer.pushState ? 'index.html' : undefined
-            }));
+        setupServer(answer);
 
-        stream.on('error', function(err) {
-            cons(err);
-        });
-
-        stream.on('close', function() {
-            cons();
-        });
-
-        stream.on('finish', function() {
-            cons();
-        });
-
-        fs.exists(lessPath, function(exists) {
-            if (!exists) {
+        fs.exists(lessPath, function(exists){
+            if (!exists){
                 return;
             }
-            gulp.watch(lessPath + '/**/*', function(event) {
+            gulp.watch(lessPath + '/**/*', function(event){
                 compileLess(lessPath, dest, paths);
             });
         });
